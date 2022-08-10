01cd9748fdc4c7845de4526e95d886dc5db6ab6b
Jani Peltonen <peltonen.jani@hotmail.fi>
2022-08-10 16:01:02 +0300

    Basic wrapper for signup and other css changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./Layout/Footer";
 import Header from "./Layout/Header";
 import FrontPage from "./Pages/FrontPage";
+import Login from "./Pages/Login/Login";
 import NotFoundPage from "./Pages/NotFoundPage";
+import Signup from "./Pages/Signup/Signup";
 
 const App = () => {
   return (
     <div className="App">
-      <Header home="home" link1="Categories" link2="Sign Up" link3="Login" />
+      <Header home="home" link1="Categories" link2="SignUp" link3="Login" />
       <Routes>
         <Route path="/" element={<FrontPage />} />
-        <Route path="/signup" />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
---
+++ src/Pages/Signup/Signup.tsx
@@ -0,0 +1,11 @@
+import styles from "./Signup.module.css";
+
+const Signup = () => {
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.main}>Signup</div>
+    </section>
+  );
+};
+
+export default Signup;
