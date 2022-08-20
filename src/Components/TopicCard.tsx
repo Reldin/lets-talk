6cbd419f3f58763d0b1691424201011a21ac6d18
Jani Peltonen <peltonen.jani@hotmail.fi>
2022-08-20 16:39:46 +0300

    Added a form for commenting to TopicCards component. Made improvements to responsiveness.

--- src/Components/TopicCard.tsx
+++ src/Components/TopicCard.tsx
@@ -9,11 +9,20 @@ const TopicCard = (props: TopicCardProps) => {
   return (
     <article className={styles.main}>
       <h1>{props.Title}</h1>
-      <div className={styles.main_post}>
+      <form className={styles.main_comment}>
+        <label htmlFor="Post">Post a Comment</label>
+        <textarea name="Post" placeholder="Enter your comment..." />
+        <button className={styles.main_comment_button}>Post</button>
+      </form>
+      <div className={styles.main_posts}>
         {props.Posts.map((post) => (
-          <p key={Math.random()}>{post}</p>
+          <div className={styles.main_posts_post} key={Math.random()}>
+            <p>{post}</p>
+            <span>Username</span>
+          </div>
         ))}
       </div>
+      <button className={styles.main_button}>Show more</button>
     </article>
   );
 };
